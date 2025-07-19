f8ba10da3ac0d3d6fbe75e343340b1fc7c97ec2a
Miguel Eduardo <dasilvamigueleduardo@gmail.com>
2025-07-19 18:14:40 -0300

    feat: project VITE creation and integration with TAILWIND

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-950 w-screen h-screen flex p-6 justify-center">
+      <h1 className="text-red-600">TEXTE</h1>
+    </div>
+  );
+}
+
+export default App;
