61cc4fb50de6f79ad09c26399da2d0b996e9141b
Eliseu Silva <eliseusilva.info@gmail.com>
2025-07-22 22:59:19 +0100

    Add register page and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import RegisterPage from "./pages/RegisterPage";
+
 function App() {
-  return (
-    <div className="bg-gray-950 w-screen h-screen flex p-6 justify-center">
-      <h1 className="text-red-600">TEXTE</h1>
-    </div>
-  );
+  return <RegisterPage />;
 }
 
 export default App;
